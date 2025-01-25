c7f2f76ec7ff0e742bb428fca87c5391893d2f84
Razabhatti94 <bhatti.raza.1994@gmail.com>
2025-01-26 02:00:52 +0500

    hackaton
    first commit

---
+++ src/api/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import FetchData from './fetchData'
+
+function Dashboard() {
+  return (
+
+    <>
+      <h1>Raza Bhatti</h1>
+    
+    {/* <FetchData /> */}
+    </>
+ )
+
+}
+
+export default Dashboard()
